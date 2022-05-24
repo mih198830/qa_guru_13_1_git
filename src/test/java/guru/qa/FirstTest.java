2cb88c681e4ceed6e75a492ab2b58666e3bbf0a0
mikhail.matskevich <mih198830@gmail.com>
2022-05-24 20:52:09 +0400

    Conflict reproduce homework

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void FirstTest() {
-        Assertions.assertFalse(5 < 0);
+        Assertions.assertTrue(5 > 0);
     }
 
     @Test
     void SecondTest() {
-        Assertions.assertFalse(1 < 0);
+        Assertions.assertFalse(1 > -1);
     }
 }
